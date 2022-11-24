0fc1377384c22ebbdb3d22446dbaf058e040c748
FelipeAya17 <andresfelipeayaroa@hotmail.com>
2022-11-23 21:12:35 -0500

    final

---
+++ biblioteca1/biblioteca1/Views/HomePage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace biblioteca1.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class HomePage : ContentPage
+    {
+        public HomePage()
+        {
+            InitializeComponent();
+        }
+
+        private async void goPrestamo(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Prestamo());
+        }
+
+        private async void goPerfil(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Perfil());
+        }
+
+        private async void goLibros(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Libros());
+        }
+
+        private async void goMultas(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Multas());
+        }
+    }
+}
